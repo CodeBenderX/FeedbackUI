09dc4790f290c1cea67eebb091e7261d99fd5ab3
Angelo <angelo.tiquio@outlook.com>
2025-07-04 19:14:01 -0400

    worked on the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
-function Header() {
+import PropTypes from 'prop-types'
+
+function Header({ text, bgColor, textColor }) {
+
   return (
-    <header>
+    <header style={{ backgroundColor: textColor, color: bgColor }}>
       <div className="container">
         <h2>Feedback UI</h2>
       </div>
     </header>
   )
+}
+
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
 }
 
 export default Header
